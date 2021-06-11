37bf3606ad8b950e3b2b3b18542ab874e3bdc59f
Basile BRUNET <bbrunet@e2r9p8.42.fr>
2021-06-11 13:02:59 +0200

    receive binary data in request body: start of new request class

---
+++ srcs/Request/binary_body/request_class.hpp
@@ -0,0 +1,151 @@
+#ifndef REQUEST_CLASS_H
+# define REQUEST_CLASS_H
+
+# include "ConfParser.hpp"
+
+# define BUF_SIZE 10000
+
+struct request_line
+{
+    std::string method;
+    std::string target;
+    std::string extension;
+    std::string query_string;
+    std::string version;
+};
+
+struct conf
+{
+        long                        max_body_size;
+        std::vector<std::string>    allow_methods;
+        std::string                 root;
+        std::vector<std::string>    index;
+        int                         autoindex;
+        errorMap                    error_pages;
+        std::vector<std::string>    cgi_allowed_ext;
+        std::string                 auth_basic;
+        std::string                 auth_basic_user_file;
+};
+
+class Request
+{
+    friend class Response;
+    friend class CgiHandler;
+    friend class Autoindex;
+    public:
+        // typedef std::pair<std::string, std::string> header;
+
+        // ATTRIBUTES
+    private:
+        // known HTTP methods
+        static std::vector<std::string> known_methods;
+        static std::vector<std::string> build_known_methods();
+        static std::vector<unsigned char> ctrl_c;
+        static std::vector<unsigned char> build_ctrl_c();
+
+        void    initHeaders(void);
+
+
+        // reading from connection
+        int fd;
+        // std::string buffer;
+        std::vector<unsigned char> buffer;
+        
+        // error
+        int error_code;
+        std::string error_message; // for debug purpose
+        
+        // storing request
+        request_line req_line;
+        // std::list<header> headers;
+        std::map<std::string, std::string> headers;
+        unsigned long body_size;
+        unsigned long chunk_size;
+        std::string body;
+        // std::vector<unsigned char> body;
+        std::string host_uri;
+        std::string host_port;
+        std::string target_uri; // concatenation of config.root and req_line.target
+        
+        // bools
+        bool chunked_encoding;
+        bool chunked_size_read;
+        bool req_line_read;
+        bool end_of_connection;
+        bool request_ready;
+
+        // config
+        conf config;
+        ServerBlock matched_serv;
+        LocationBlock matched_loc;
+        sockaddr_in address; // ip and port of the server connected to clientwho sent the request in order to find suitable server_block
+        const std::vector<ServerBlock> &servers;
+        const HttpBlock &base_config;
+
+        // METHODS
+
+        // "storing" methods
+        void store_req_line(std::string line);
+        void store_header(std::string line);
+        void store_body_headers();
+        void store_body_size();
+        void store_chunk_size(std::string line);
+        void store_encoding();
+        void store_host();
+        bool has_transfer_encoding() const;
+        bool has_content_length() const;
+        bool has_host() const;
+
+        // main parsing methods
+        void parse_buffer();
+        void parse_req_line();
+        void parse_headers();
+        void parse_body();
+        void parse_body_normal();
+        void parse_body_chunked();
+        bool parse_chunked_size();
+        bool parse_chunked_data();
+        bool read_buf_line(std::string &line);
+        void read_from_socket();
+
+        // config
+        void init_config(); //Called in Request::parse_headers()
+        void fill_conf();
+        void match_server();
+        void match_location();
+
+    
+    public:
+        // constructor & destructor
+        Request(void); // not defined
+        Request(int fd, sockaddr_in addr, const std::vector<ServerBlock> &servers, const HttpBlock &base_config);
+        Request(const Request &copy); // not defined (needed ?)
+        virtual ~Request(void);
+        Request&  operator=(const Request &copy); // not defined
+
+        // main functions
+        void parse();
+        // void reset();
+        // utils
+        void print(void) const;
+        void print2(void) const;
+        // getters & setters
+        int get_error_code() const;
+        int get_fd() const;
+        sockaddr_in get_addr() const;
+        bool request_is_ready() const;
+        std::map<std::string, std::string> const& get_headers() const;
+        std::map<int, std::string> const&     getErrorPages(void) const;
+        bool connection_end() const;
+        // void set_error_code(int code);
+
+};
+
+bool content_length_present(std::pair<std::string, std::string> header);
+bool transfer_encoding_present(std::pair<std::string, std::string> header);
+bool host_present(std::pair<std::string, std::string> header);
+
+std::ostream & operator<<(std::ostream & o, Request const & rhs);
+
+
+#endif
---
+++ srcs/Request/binary_body/request_class_utils.cpp
@@ -0,0 +1,174 @@
+#include "request_class.hpp"
+
+std::vector<std::string> Request::build_known_methods()
+{
+    std::vector<std::string> methods;
+    methods.push_back("GET");
+    methods.push_back("HEAD");
+    methods.push_back("POST");
+    methods.push_back("PUT");
+    methods.push_back("DELETE");
+    methods.push_back("OPTIONS");
+    methods.push_back("TRACE");
+    // methods.push_back("CONNECT");
+    // methods.push_back("PATCH");
+    return methods;
+}
+
+void                    Request::initHeaders(void)
+{
+    this->headers["accept-charsets"] = "";
+    this->headers["accept-language"] = "";
+    this->headers["allow"] = "";
+    this->headers["auth-scheme"] = "";
+    this->headers["authorization"] = "";
+    this->headers["content-language"] = "";
+    this->headers["content-length"] = "";
+    this->headers["content-location"] = "";
+    this->headers["content-type"] = "";
+    this->headers["date"] = "";
+    this->headers["host"] = "";
+    this->headers["last-modified"] = "";
+    this->headers["location"] = "";
+    this->headers["referer"] = "";
+    this->headers["retry-after"] = "";
+    this->headers["server"] = "";
+    this->headers["transfer-encoding"] = "";
+    this->headers["user-agent"] = "";
+    this->headers["www-authenticate"] = "";
+    this->headers["connection"] = "";
+}
+
+std::vector<std::string> Request::known_methods = Request::build_known_methods();
+
+std::vector<unsigned char> Request::ctrl_c = Request::build_ctrl_c();
+
+std::vector<unsigned char> Request::build_ctrl_c()
+{
+    std::vector<unsigned char> vec;
+    vec.push_back('\xFF');
+    vec.push_back('\xF4');
+    vec.push_back('\xFF');
+    vec.push_back('\x06');
+    return vec;
+        // if (ret == 5 && strcmp(buf, "\xFF\xF4\xFF\xFD\x06") == 0)
+}
+
+
+// // reset is called before each request, to "clear" the previous request and init attributes (it is also called for the first request)
+// void Request::reset()
+// {
+//     std::cout << "reseting request" << std::endl;
+//     // set bools to false
+//     this->chunked_encoding = false;
+//     this->req_line_read = false;
+//     this->request_ready = false;
+//     this->chunked_size_read = false;
+//     // reset req line, headers & body
+//     this->req_line.method = "";
+//     this->req_line.version = "";
+//     this->req_line.target = "";
+//     this->headers.erase(this->headers.begin(), this->headers.end());
+//     this->body = "";
+//     this->body_size = 0;
+//     this->chunk_size = 0;
+//     // this->host_port = 0;
+//     this->host_uri = "";
+//     // config
+//     this->config.root = "";
+//     this->config.max_body_size = NOT_SET;
+//     this->config.autoindex = NOT_SET;
+//     this->config.index.erase(this->config.index.begin(), this->config.index.end());
+//     this->config.allow_methods.erase(this->config.allow_methods.begin(), this->config.allow_methods.end());
+//     this->config.error_pages.erase(this->config.error_pages.begin(), this->config.error_pages.end());
+
+//     // reset serverblock and location block
+// }
+
+int Request::get_error_code() const
+{
+    return this->error_code;
+}
+
+int Request::get_fd() const
+{
+    return this->fd;
+}
+
+sockaddr_in Request::get_addr() const
+{
+    return this->address;
+}
+
+bool Request::connection_end() const
+{
+    return this->end_of_connection;
+}
+
+bool Request::request_is_ready() const
+{
+    return this->request_ready;
+}
+
+std::map<std::string, std::string> const& Request::get_headers() const
+{
+    return this->headers;
+} 
+
+std::map<int, std::string> const&     Request::getErrorPages(void) const
+{
+    return this->config.error_pages;
+}
+
+
+// void Request::set_error_code(int code)
+// {
+//     this->error_code = code;
+// };
+
+void Request::print() const
+{
+    if (this->error_code)
+    {
+        std::cout << "Error:" << error_code << std::endl;
+        std::cout << error_message << std::endl;
+        return ;
+    }
+    std::cout << "Request line:" << std::endl;
+    std::cout << " . Method: " << this->req_line.method << std::endl;
+    std::cout << " . Target: " << this->req_line.target << std::endl;
+    std::cout << " . Version: " << this->req_line.version << std::endl;
+    std::cout << std::endl;
+    for (std::map<std::string, std::string>::const_iterator it = this->headers.begin(); it != this->headers.end(); it++)
+    {
+        std::cout << "Header line:" << std::endl;
+        std::cout << " . field_name: " << "[" << it->first << "]" << std::endl;
+        std::cout << " . field_value: " << "[" << it->second << "]" << std::endl;
+        std::cout << std::endl;
+    }
+    if (!this->body.empty())
+    {
+        std::cout << "body: " << this->body << std::endl;
+        std::cout << std::endl;
+    }
+}
+
+void    Request::print2(void) const
+{
+    std::cout << "REQUEST DETAILS: " << std::endl;
+    std::cout << "--------------------------------" << std::endl;
+
+    std::cout << "HOST: " << this->host_uri << std::endl;
+    std::cout << "PORT: " << this->host_port << std::endl;
+    std::cout << "METHOD: " << this->req_line.method << std::endl;
+    std::cout << "TARGET: " << this->req_line.target << std::endl;
+    std::cout << "VERSION: " << this->req_line.version << std::endl;
+    std::cout << "TARGET PATH: " << this->target_uri << std::endl;
+    
+    std::cout << "BODY: " << this->body << std::endl;
+    std::cout << "BDY SIZE: " << iToString(this->body_size) << std::endl;
+    
+    std::cout << "CHUNK SIZE: " << iToString(this->chunk_size) << std::endl;
+
+    std::cout << "--------------------------------" << std::endl;
+}
